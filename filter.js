b44cd1a67b26e005dfcb0ee0a19ba14b751b79a7
Mydulislam <mydulislam693852@gmail.com>
2023-08-06 19:56:38 +0600

    product-category-filtering

---
+++ filter.js
@@ -0,0 +1,31 @@
+const btns = [
+    {
+        id:1,
+        name:'Mobiles'
+    },
+    {
+        id:2,
+        name:'Watches'
+    },
+    {
+        id:3,
+        name:'Cameras'
+    },
+    {
+        id:4,
+        name:'Laptops'
+    },
+    {
+        id:5,
+        name:'Air Pods'
+    }
+]
+const filters = [...new Set(btns.map((btn)=>{
+    {return btn}
+}))]
+document.getElementById('btns').innerHTML = filters.map((btn)=>{
+    var {name, id} = btn;
+    return(
+        "<button class= 'fil-p' onclick='filterItems("+(id)+`)'>${name}</button>`
+    )
+}).join('');
